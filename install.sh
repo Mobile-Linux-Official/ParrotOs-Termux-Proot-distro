b6c089f9665dea1ee4c3d72d7d4a6e9f03dbc388
Mobile-Linux-Official <108542674+Mobile-Linux-Official@users.noreply.github.com>
2022-07-16 23:16:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "DISTRO_NAME="ParrotOs"
 
 TARBALL_URL['aarch64']="https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Parrot/arm64/parrot-rootfs-arm64.tar.xz"
 TARBALL_SHA256['aarch64']="d563b790ef367d9857d928de8fb967116561d4050cc00d0ffd58390be32641c6"
-TARBALL_URL['arm']="https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Rootfs/Parrot/armhf/parrot-rootfs-armhf.tar.xz"
+TARBALL_URL['arm']="https://github.com/Mobile-Linux-Official/ParrotOs-Termux-Proot-distro/raw/main/parrot-fs/parrot-rootfs-armhf.tar.xz"
 TARBALL_SHA256['arm']="ad5352df583e315869b12e5eb4fa3205b0d7a4284216f6d6d3e4d9a87209b12c"
 
 
